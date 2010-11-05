baa6cbb3163d0d0a17c5c7d0d4c4e146b56a952a
Henrik Hautakoski <henrik@fiktivkod.org>
2010-11-05 11:13:18 +0100

    docs: index generation

---
+++ docs/genindex.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    exit 1
+fi
+
+(
+    echo "Archived Documentation"
+    echo "----------------------"
+
+    for DOC in `ls *.txt`; do
+        if [ "$DOC" = "$1" ]; then
+            continue
+        fi
+        display=$(sed -e 1q "${DOC}")
+        link=${DOC%.txt}.html
+        echo "* link:${link}[${display}]"
+    done
+) > $1

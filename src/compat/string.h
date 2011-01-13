51dd9eac435cd5176e90424403cd7decc3464621
Henrik Hautakoski <henrik@fiktivkod.org>
2011-01-13 14:54:19 +0100

    compat/string.h: Added header-guards

--- src/compat/string.h
+++ src/compat/string.h
@@ -1,6 +1,11 @@
 
+#ifndef __COMPAT_STRING_H
+#define __COMPAT_STRING_H
+
 #include <string.h>
 
 #ifdef NO_MEMRCHR
 extern void* memrchr(const void *, int, size_t);
 #endif
+
+#endif /* __COMPAT_STRING_H */
